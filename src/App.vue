127fbfdc75877b7d8661cf1d45db2ffe469e3d23
Shaik Subhan <shaik@appspiresolutions.in>
2026-07-21 18:53:03 +0530

    Added vue learning files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 
+import Day_1 from "./Day_1.vue";
 import Day_2 from "./Day_2.vue";
+import Day_3 from "./Day_3.vue";
 </script>
 
 <template>
-  <Day_2 />
-
+  <Day_1 />
+ <Day_2 />
+ <Day_3 />
 </template>
---
+++ src/Day_3.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, reactive, toRefs } from 'vue'
+
+const name = ref("subhan")
+
+function changename() {
+  name.value = "sami"
+}
+
+const age = ref(20)
+
+const user = reactive({
+  name: "subhan",
+  age: 22
+})
+
+function changeuser() {
+  user.name = "sami"
+  user.age = 21
+}
+
+const users = reactive({
+  name: "Alice",
+  age: 30
+})
+
+const { name: userName, age: userAge } = toRefs(users)
+
+function changeNames() {
+  userName.value = "Bob"
+}
+</script>
+
+<template>
+  <h2>{{ name }}</h2>
+  <button @click="changename">Change Name</button>
+
+  <h3>{{ age }}</h3>
+  <button @click="age++">Increment</button>
+  <button @click="age--">Decrement</button>
+
+  <h2>User Object</h2>
+
+  <h3>Name : {{ user.name }}</h3>
+  <h3>Age : {{ user.age }}</h3>
+
+  <button @click="changeuser">
+    Change User
+  </button>
+
+  <hr>
+
+  <h2>toRefs Example</h2>
+
+  <h3>{{ userName }}</h3>
+  <h3>{{ userAge }}</h3>
+
+  <button @click="changeNames">
+    Change Name using toRefs
+  </button>
+</template>
